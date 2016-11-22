c2092af836b2fbd31936a9593d92a9bdca8eb648
Setény János <seteny.janos@tigra.hu>
2016-11-22 13:37:59 +0100

    TDD step 4: int[][] field (an array to store the values of the cells)

--- minesweeper/src/main/java/hu/tigra/minesweeper/MineField.java
+++ minesweeper/src/main/java/hu/tigra/minesweeper/MineField.java
@@ -3,18 +3,36 @@ package hu.tigra.minesweeper;
 
 public class MineField {
 
-    private int count;
+    private int[][] field;
+
+    private final int width;
+    private final int height;
 
 
     public MineField(int width, int height) {
-
+        this.width = width;
+        this.height = height;
+        field = new int[width][height];
     }
 
     public void addBomb(int x, int y) {
-        ++count;
+        field[x][y] = -9;
+        incrementNeighbours(x, y);
+    }
+
+    private void incrementNeighbours(int bx, int by) {
+        for (int y = Math.max(0, by - 1); y <= Math.min(height - 1, by + 1); ++y) {
+            for (int x = Math.max(0, bx - 1); x <= Math.min(width - 1, bx + 1); ++x) {
+                if (x != bx || y != by) {
+                    field[x][y] = field[x][y] + 1;
+                }
+            }
+        }
+
+        // TODO field = field.stream().map()
     }
 
     public int at(int x, int y) {
-        return (x < 2) ? count : 1;
+        return field[x][y];
     }
 }
