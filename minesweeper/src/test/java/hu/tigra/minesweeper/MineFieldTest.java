d1e4c1a9b92a6ea466c5bc86449996ac7a218b27
Setény János <seteny.janos@tigra.hu>
2016-11-23 12:37:31 +0100

    TDD step 5: refactor the test and replace assertEquals("1 at 1,0", 1, mineField.at(1, 0)); with assertAt(1, 0, mineField, has(1));
    https://en.wikipedia.org/wiki/Don't_repeat_yourself

--- minesweeper/src/test/java/hu/tigra/minesweeper/MineFieldTest.java
+++ minesweeper/src/test/java/hu/tigra/minesweeper/MineFieldTest.java
@@ -15,9 +15,9 @@ public class MineFieldTest {
         mineField.addBomb(0, 0);
 
         // Then
-        assertEquals("1 at 1,0", 1, mineField.at(1, 0));
-        assertEquals("1 at 0,1", 1, mineField.at(0, 1));
-        assertEquals("1 at 1,1", 1, mineField.at(1, 1));
+        assertAt(1, 0, mineField, has(1));
+        assertAt(0, 1, mineField, has(1));
+        assertAt(1, 1, mineField, has(1));
     }
 
     @Test
@@ -30,18 +30,35 @@ public class MineFieldTest {
         mineField.addBomb(1, 0);
 
         // Then
-        assertEquals("1 at 2,0", 1, mineField.at(2, 0));
-        assertEquals("2 at 0,1", 2, mineField.at(0, 1));
-        assertEquals("2 at 1,1", 2, mineField.at(1, 1));
-        assertEquals("1 at 2,1", 1, mineField.at(2, 1));
+        assertAt(2, 0, mineField, has(1));
+        assertAt(0, 1, mineField, has(2));
+        assertAt(1, 1, mineField, has(2));
+        assertAt(2, 1, mineField, has(1));
     }
 
     @Test
     public void add_2_neighborBombs_somewhere() {
         // Given
+        MineField mineField = new MineField(10, 7);
 
         // When
+        mineField.addBomb(4, 3);
+        mineField.addBomb(5, 3);
 
         // Then
+        assertAt(3, 2, mineField, has(1));
+        assertAt(4, 2, mineField, has(2));
+        assertAt(5, 2, mineField, has(2));
+        assertAt(6, 2, mineField, has(1));
+
+        // TODO check more neighbours
+    }
+
+    private void assertAt(int x, int y, MineField mineField, int expected) {
+        assertEquals("expected " + expected + " at " + x + "," + y, expected, mineField.at(x, y));
+    }
+
+    private int has(int n) {
+        return n;
     }
 }
