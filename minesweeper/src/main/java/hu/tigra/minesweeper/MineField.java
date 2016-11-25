954bebdb763ead55d26dbc3b8794b0077efe6fd7
jseteny <jseteny@gmail.com>
2016-11-25 01:14:55 +0100

    java8 functional programming solution

--- minesweeper/src/main/java/hu/tigra/minesweeper/MineField.java
+++ minesweeper/src/main/java/hu/tigra/minesweeper/MineField.java
@@ -1,10 +1,12 @@
 package hu.tigra.minesweeper;
 
+import java.util.stream.IntStream;
+
 
 public class MineField {
 
     public static final int BOMB = -9;
-    private int[][] field;
+    private Integer[][] field;
 
     private final int width;
     private final int height;
@@ -13,30 +15,29 @@ public class MineField {
     public MineField(int width, int height) {
         this.width = width;
         this.height = height;
-        field = new int[height][width];
+        field = IntStream.range(0, height).mapToObj(y ->
+                IntStream.range(0, width).mapToObj(x ->
+
+                        0
+
+                ).toArray(size -> new Integer[width]))
+                .toArray(size -> new Integer[height][width]);
     }
 
-    public void addBomb(int x, int y) {
-        field[y][x] = BOMB;
-        incrementNeighbours(x, y);
-    }
-
-    private void incrementNeighbours(int bombX, int bombY) {
-        int startY = Math.max(0, bombY - 1);
-        int endY = Math.min(height - 1, bombY + 1);
-
-        int startX = Math.max(0, bombX - 1);
-        int endX = Math.min(width - 1, bombX + 1);
-
-        for (int y = startY; y <= endY; ++y) {
-            for (int x = startX; x <= endX; ++x) {
-                if (field[y][x] != BOMB) {
-                    field[y][x] = field[y][x] + 1;
-                }
-            }
-        }
-
-        // TODO field = field.stream().map()
+    public void addBomb(int bombX, int bombY) {
+        field = IntStream.range(0, height).mapToObj(y ->
+                IntStream.range(0, width).mapToObj(x -> {
+                    if (x == bombX && y == bombY) {
+                        return BOMB;
+                    } else if (field[y][x] == BOMB) {
+                        return BOMB;
+                    } else if (Math.abs(x - bombX) <= 1 && Math.abs(y - bombY) <= 1) {
+                        return field[y][x] + 1;
+                    } else {
+                        return field[y][x];
+                    }
+                }).toArray(size -> new Integer[width]))
+                .toArray(size -> new Integer[height][width]);
     }
 
     public int at(int x, int y) {
