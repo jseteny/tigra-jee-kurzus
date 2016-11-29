fdfeb9a84650f805405e80aa4534fb59727defc2
Setény János <seteny.janos@tigra.hu>
2016-11-29 11:40:35 +0100

    unmaskAt_00 fails if unmask should have been recursive

--- minesweeper/src/test/java/hu/tigra/minesweeper/MaskTest.java
+++ minesweeper/src/test/java/hu/tigra/minesweeper/MaskTest.java
@@ -14,28 +14,54 @@ public class MaskTest {
     @Before
     public void setup() {
         int[][] field = {
-                {0, 0, 1, 1},
-                {0, 0, 1, B},
-                {1, 2, 2, 2},
-                {B, 2, B, 1}
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 2, B},
+                {0, 0, 0, 2, B},
+                {1, 1, 2, 2, 2},
+                {1, B, 2, B, 1}
         };
         mineField = new MineField(field);
+    }
+
+    @Test
+    public void unmaskAt_11() {
+
+        int[][] previous = {
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+        };
+        int[][] expected = {
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+        };
+        int[][] actual = Mask.unmask(mineField, previous, 1, 1);
+        for (int r = 0; r < expected.length; r++) {
+            assertThat(actual[r]).as("row " + r).containsExactly(expected[r]);
+        }
     }
 
     @Test
     public void unmaskAt_00() {
 
         int[][] previous = {
-                {1, 1, 1, 1},
-                {1, 1, 1, 1},
-                {1, 1, 1, 1},
-                {1, 1, 1, 1}
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
         };
         int[][] expected = {
-                {0, 0, 1, 1},
-                {0, 0, 1, 1},
-                {1, 1, 1, 1},
-                {1, 1, 1, 1}
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
         };
         int[][] actual = Mask.unmask(mineField, previous, 0, 0);
         for (int r = 0; r < expected.length; r++) {
