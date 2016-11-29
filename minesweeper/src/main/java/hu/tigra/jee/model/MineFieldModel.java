18e4cc5f76f30e1dcd4e9c584a6793ca3291f7d9
Setény János <seteny.janos@tigra.hu>
2016-11-29 16:38:24 +0100

    Works for zeros

--- minesweeper/src/main/java/hu/tigra/jee/model/MineFieldModel.java
+++ minesweeper/src/main/java/hu/tigra/jee/model/MineFieldModel.java
@@ -1,5 +1,7 @@
 package hu.tigra.jee.model;
 
+import hu.tigra.minesweeper.Mask;
+import hu.tigra.minesweeper.Mask.Explosion;
 import hu.tigra.minesweeper.MineField;
 
 import javax.enterprise.inject.Model;
@@ -13,19 +15,38 @@ import java.util.stream.Stream;
 public class MineFieldModel {
 
     private MineField mineField;
+    private int[][] mask;
 
     private List<Integer> collumns;
     private List<MineFieldRow> rows;
 
+    @SuppressWarnings("UnnecessaryLocalVariable")
     public MineFieldModel() {
         mineField = new MineField(8, 5);
         mineField.addBomb(4, 3);
         mineField.addBomb(5, 3);
 
+        int[][] m = {
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1}
+        };
+        mask = m;
+
         collumns = IntStream.range(0, mineField.getWidth() - 1).boxed().collect(Collectors.toList());
 
         final int[] rowCount = {0};
         rows = Stream.generate(() -> new MineFieldRow(rowCount[0]++)).limit(mineField.getHeight()).collect(Collectors.toList());
+    }
+
+    public void click(int x, int y) {
+        try {
+            mask = Mask.unmask(mineField, mask, x, y);
+        } catch (Explosion explosion) {
+            // TODO
+        }
     }
 
     public List<Integer> getColumns() {
@@ -46,8 +67,12 @@ public class MineFieldModel {
             this.r = r;
         }
 
-        public int cell(int c) {
-            return mineField.at(c, r);
+        public String cell(int c) {
+            return (mask[r][c] == 0) ? String.valueOf(mineField.at(c, r)) : "";
+        }
+
+        public int getIndex() {
+            return r;
         }
     }
 }
